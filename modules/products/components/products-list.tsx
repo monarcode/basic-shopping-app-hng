5f81fca497ad82ec22785defdb00353e4c2067d7
monarcode <ugbahisioma@gmail.com>
2024-07-02 23:44:08 +0100

    fix: updated styling to account for scroll

--- modules/products/components/products-list.tsx
+++ modules/products/components/products-list.tsx
@@ -5,18 +5,22 @@ import ProductCard from "./product-card";
 
 const ProductsList = () => {
   return (
-    <FlatList
-      keyExtractor={({ id }) => id.toString()}
-      renderItem={({ item }) => <ProductCard product={item} />}
-      data={products}
-      numColumns={2}
-      contentContainerStyle={{
-        rowGap: THEME.spacer * 6,
-      }}
-      columnWrapperStyle={{
-        justifyContent: "space-between",
-      }}
-    />
+    <>
+      <FlatList
+        keyExtractor={({ id }) => id.toString()}
+        renderItem={({ item }) => <ProductCard product={item} />}
+        data={products}
+        numColumns={2}
+        contentContainerStyle={{
+          rowGap: THEME.spacer * 6,
+          paddingBottom: THEME.spacer * 4,
+        }}
+        columnWrapperStyle={{
+          justifyContent: "space-between",
+        }}
+        showsVerticalScrollIndicator={false}
+      />
+    </>
   );
 };
 export default ProductsList;
