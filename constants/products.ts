e2df698db17a75d7e20cdba491b27ca7d45376d9
monarcode <ugbahisioma@gmail.com>
2024-07-02 23:43:45 +0100

    chore: added more products

--- constants/products.ts
+++ constants/products.ts
@@ -16,4 +16,19 @@ export const products: TProduct[] = [
     name: "iPhone 14 Pro Max",
     price: 1299,
   },
+  {
+    id: 4,
+    name: "iPhone 15",
+    price: 1299,
+  },
+  {
+    id: 5,
+    name: "iPhone 15 Pro",
+    price: 1599,
+  },
+  {
+    id: 6,
+    name: "iPhone 15 Pro Max",
+    price: 1999,
+  },
 ];
